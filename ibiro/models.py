04de153ffa985f1ece8465d6c72c1252193857bc
je-ss-y <Nkujess@gmail.com>
2019-11-01 15:39:30 +0200

    added admin

--- ibiro/models.py
+++ ibiro/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Location(models.Model):
+    umujyi = models.CharField(max_length =30)
+    umurenge = models.CharField(max_length =30)
+    
+
+    def __str__(self):
+        return self.first_name
+
+    def save_editor(self):
+        self.save()
+
+
+    def save_editor(self):
+        self.delete()
+
--- ibiro/tests.py
+++ ibiro/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Location
 
-# Create your tests here.
+
+class LocationTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.james= Location(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.james,Location))
+
+
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.james.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+     # Testing Save Method
+    def test_delete_method(self):
+        self.james.delete_editor()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) ==0)
