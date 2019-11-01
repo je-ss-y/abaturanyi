e9182dcc363d26b65b39c7dae4f0eb72bdd648da
je-ss-y <Nkujess@gmail.com>
2019-11-01 16:58:29 +0200

    added search bar

--- ibiro/views.py
+++ ibiro/views.py
@@ -10,3 +10,16 @@ from .models import Location
 def welcome(request):
     location = Location.objects.all()
     return render(request, 'all-posts/welcome.html',{"location":location})
+
+def search_results(request):
+
+    if 'username' in request.GET and request.GET["username"]:
+        search_term = request.GET.get("username")
+        searched_users= Image.search_by_name(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-posts/search.html',{"searched_users": searched_users})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-posts/search.html',{"message":message})
