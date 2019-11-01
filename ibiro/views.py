6a9fe59c1b05865175180a45dcd8be196751bf41
je-ss-y <Nkujess@gmail.com>
2019-11-01 17:18:39 +0200

    added nav bar

--- ibiro/views.py
+++ ibiro/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 from .models import Location
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 
 
 
+
+@login_required(login_url='/accounts/login/')
 def welcome(request):
     location = Location.objects.all()
     return render(request, 'all-posts/welcome.html',{"location":location})
