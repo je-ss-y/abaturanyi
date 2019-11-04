31daac3b75348e0abd2398dbf119438d6aac4a79
je-ss-y <Nkujess@gmail.com>
2019-11-04 09:51:11 +0200

    url of post

--- ibiro/urls.py
+++ ibiro/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^profileform', views.profile_form, name='profile'),
     url(r'^profiledisplay', views. user_profile, name='profiledisplay'),
+    url(r'^new/post$', views.new_post, name='new_post'),
 ]
